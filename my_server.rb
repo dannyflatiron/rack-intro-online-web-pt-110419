9172b1ca93977731c334a6045ed1e834b3b535eb
Daniel Reina <dannyflatiron@gmail.com>
2020-02-12 19:17:16 +0000

    Automatically backed up by Learn

---
+++ my_server.rb
@@ -0,0 +1,10 @@
+class MyServer
+  def call(envy)
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+end
+
+  def pretty_response
+    (Time.now.to_i % 2).zero? ? ["<em>Hello my name is Danny</em>"] : ["<strong>Hello my name is Danny</strong>"]
+  end
+
+end
