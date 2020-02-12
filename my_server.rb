cbcfd4d37f3e844f9f1cbcc8996bf6b76efb2178
Daniel Reina <dannyflatiron@gmail.com>
2020-02-12 19:20:45 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -4,7 +4,7 @@ class MyServer
 end
 
   def pretty_response
-    (Time.now.to_i % 2).zero? ? ["<em>Hello my name is Danny</em>"] : ["<strong>Hello my name is Danny</strong>"]
+    (Time.now.to_i % 2).zero? ? ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 
 end
